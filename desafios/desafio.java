763fb7bcb40af97f6afb7d310cbbcf43ba10a84a
Arcanodev <92180339+kaiolabs@users.noreply.github.com>
2022-04-14 16:30:04 -0300

    Update

--- desafios/desafio.java
+++ desafios/desafio.java
@@ -1,11 +1,38 @@
 package desafios;
 
+import java.util.Scanner;
+
 public class desafio {
     public static void main(String[] args){
 
-       int cal = 2022 % 19;
-       int cal2 = cal + 1;
-       System.out.println(cal2);
+        Scanner in = new Scanner(System.in);
+
+        int ano, d, dia_da_pascoa, e, mes_da_pascoa;
+        System.out.print("Digite o valor do ano: ");
+        ano = in.nextInt();
+        in.nextLine();
+        d=(19*(ano%19)+24)%30;
+        e=(2*(ano%4)+4*(ano%7)+6*d+5)%7;
+        if(d+e<10)
+            dia_da_pascoa=d+e+22;
+        else
+            dia_da_pascoa=d+e-9;
+        if(d+e<10)
+        {
+            mes_da_pascoa=3;
+            System.out.println("Mar\u00E7o");
+        }
+        else
+        {
+            mes_da_pascoa=4;
+            System.out.println("Abril");
+        }
+        if(dia_da_pascoa==26&&mes_da_pascoa==4)
+            dia_da_pascoa=19;
+        if(dia_da_pascoa==25&&mes_da_pascoa==4&&d==28&&e==6&&(ano%19)>10)
+            dia_da_pascoa=18;
+        System.out.println("O valor do dia da pascoa: " + dia_da_pascoa);
+        System.out.println("O valor do mes da pascoa: " + mes_da_pascoa);
 
     }
 
